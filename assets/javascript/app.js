8a9c437093a5239d14b874147e7c7d87664f2167
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-03-13 21:19:44 -0400

    fixed bug with the number of gifs displayed. Some were getting filtered due to rating condition

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,6 +18,81 @@ function renderButtons(takeTopicsArray) {
 //call this function so that the buttons are on there
 renderButtons(topics);
 
+// main function to add img tags with gifs and control animate
+function makeAJAXcall() {
+
+    // to clear existing gifs
+    $("#gifs-appear-here").empty();
+
+    var searchtv = $(this).attr("data-name");
+    console.log(searchtv);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        searchtv + "&api_key=obrCpu0efOsCa5DrJgBmhBewTGDBxXHh&limit=20";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        var results = response.data;
+        console.log(results);
+
+        var countOfGifs=0;
+
+        // for the response array, add rating and image to a div, then append that to page
+        for (var i = 0; i < results.length; i++) {
+
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13" && countOfGifs< 10) {
+                countOfGifs++;
+
+                var gifDiv = $("<div>");
+
+                var rating = results[i].rating;
+
+                var pForRating = $("<p>").text("Rating: " + rating);
+
+                var tvImage = $("<img>");
+
+                // add src as still image, and other attributes to hold still vs animated gif and its state
+                tvImage.attr("src", results[i].images.fixed_height_still.url);
+                tvImage.attr("data-still", results[i].images.fixed_height_still.url);
+                tvImage.attr("data-animate", results[i].images.fixed_height.url);
+                tvImage.attr("data-state", "still");
+                tvImage.addClass("gif");
+
+                gifDiv.append(pForRating);
+                gifDiv.append(tvImage);
+
+                $("#gifs-appear-here").prepend(gifDiv);
+                
+            }
+        }
+
+        // click listener to animate or still the gif
+        $(".gif").on("click", function () {
+            console.log("this has been clicked")
+            var state = $(this).attr("data-state");
+            console.log(state);
+
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
+
+
+
+
+    });
+
+};
+
+
+
 // on click listener on the submit form element 
 // 1. stop default action
 // 2. if not in array then push
@@ -34,70 +109,14 @@ $("#add-tv").on("click", function (event) {
     }
 
     renderButtons(topics);
+
+    // on click for new buttons added and then make an API call
+    $("button").on("click", makeAJAXcall);
+
 });
 
-// on click listener on the buttons
-// ajax call
-// 
-$("button").on("click", function () {
-
-    var searchtv = $(this).attr("data-name");
-    console.log(searchtv);
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        searchtv + "&api_key=obrCpu0efOsCa5DrJgBmhBewTGDBxXHh&limit=10";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-            
-            var results = response.data;
-            console.log(results);
-
-            // for the response array, add rating and image to a div, then append that to page
-            for (var i = 0; i < results.length; i++) {
-
-                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                    
-                    var gifDiv = $("<div>");
-
-                    var rating = results[i].rating;
-                    
-                    var pForRating = $("<p>").text("Rating: " + rating);
-                    
-                    var tvImage = $("<img>");
-                    
-                    // add src as still image, and other attributes to hold still vs animated gif and its state
-                    tvImage.attr("src", results[i].images.fixed_height_still.url);
-                    tvImage.attr("data-still", results[i].images.fixed_height_still.url);
-                    tvImage.attr("data-animate", results[i].images.fixed_height.url);
-                    tvImage.attr("data-state","still");
-                    tvImage.addClass("gif");
-                    
-                    gifDiv.append(pForRating);
-                    gifDiv.append(tvImage);
-                    
-                    $("#gifs-appear-here").prepend(gifDiv);
-                }
-            }
-
-            // click listener to animate or still the gif
-            $(".gif").on("click", function() {
-                console.log("this has been clicked")
-                var state = $(this).attr("data-state");
-                console.log(state);
-                
-                if (state === "still") {
-                  $(this).attr("src", $(this).attr("data-animate"));
-                  $(this).attr("data-state", "animate");
-                } else {
-                  $(this).attr("src", $(this).attr("data-still"));
-                  $(this).attr("data-state", "still");
-                }
-              });
+// on click listener on the existing buttons to make api call and display to page
+$("button").on("click", makeAJAXcall);
 
 
-        });
-});
 
