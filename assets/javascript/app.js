bcfa7faf3d5af04941fdd77d162742eb8d28f69e
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-03-13 20:09:22 -0400

    make ajax call to get 10 gifs with rating and display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,14 +22,14 @@ renderButtons(topics);
 // 1. stop default action
 // 2. if not in array then push
 // 3. render all buttons
-$("#add-tv").on("click", function(event){
+$("#add-tv").on("click", function (event) {
     event.preventDefault();
 
     //get text from the text box
     var tv = $("#tv-input").val().trim();
-    
+
     //check if tv show exists in the array already
-    if (topics.includes(tv)==false){
+    if (topics.includes(tv) == false) {
         topics.push(tv);
     }
 
@@ -39,3 +39,48 @@ $("#add-tv").on("click", function(event){
 // on click listener on the buttons
 // ajax call
 // 
+$("button").on("click", function () {
+
+    var searchtv = $(this).attr("data-name");
+    console.log(searchtv);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        searchtv + "&api_key=obrCpu0efOsCa5DrJgBmhBewTGDBxXHh&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+            
+            var results = response.data;
+            console.log(results);
+
+            
+            for (var i = 0; i < results.length; i++) {
+
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    
+                    var gifDiv = $("<div>");
+
+                    var rating = results[i].rating;
+                    
+                    var pForRating = $("<p>").text("Rating: " + rating);
+                    
+                    var tvImage = $("<img>");
+                    
+                    // add src as still image, and other attributes to hold still vs animated gif and its state
+                    tvImage.attr("src", results[i].images.fixed_height_still.url);
+                    tvImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    tvImage.attr("data-animate", results[i].images.fixed_height.url);
+                    tvImage.attr("data-state","still");
+                    tvImage.addClass("gif");
+                    
+                    gifDiv.append(pForRating);
+                    gifDiv.append(tvImage);
+                    
+                    $("#gifs-appear-here").prepend(gifDiv);
+                }
+            }
+        });
+});
+
